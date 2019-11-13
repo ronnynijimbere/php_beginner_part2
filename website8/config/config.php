45d90d58f8212f41fb5f90ded49f994a54409be6
Ronny Nijimbere <50517839+ronnynijimbere@users.noreply.github.com>
2019-11-13 15:45:12 +0200

    Update config.php
    password changed

--- website8/config/config.php
+++ website8/config/config.php
@@ -2,7 +2,7 @@
     define('ROOT_URL', 'http://localhost/php_front_back/website8/');
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
-    define('DB_PASS', 'jimbere21');
+    define('DB_PASS', 'test1234');
     define('DB_NAME', 'phpblog');
   
 ?>
